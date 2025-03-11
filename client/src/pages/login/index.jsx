4a560b7d733ba1316c72dfb174bf995321866ec2
salhZvolv <nothingism99@gmail.com>
2025-03-11 11:50:27 +0300

    working on ui of the login page

--- client/src/pages/login/index.jsx
+++ client/src/pages/login/index.jsx
@@ -1,13 +1,40 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { loginUser } from "./../../apiCalls/auth";
+import { toast } from "react-hot-toast";
+
 const Login = () => {
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
-  const onFormSubmit = (event) => {
+  const onFormSubmit = async (event) => {
     event.preventDefault();
     console.log(user);
+    try {
+      const resData = await loginUser(user);
+      if (resData.status === "SUCCESS") {
+        toast.success(resData.token);
+        localStorage.setItem("token", resData.token);
+        window.location.href = "/";
+      } else {
+        let errorMessage = "An unexpected error occurred.";
+        // Check if the error is related to the database
+        if (resData.message.includes("getaddrinfo ENOTFOUND")) {
+          errorMessage =
+            "Unable to connect to the server. Please check your network connection.";
+        } else {
+          errorMessage = resData.message || errorMessage;
+        }
+        console.log(resData);
+        console.log("reached here");
+        toast.error(errorMessage);
+      }
+    } catch (error) {
+      const res = "unexpected error occured, try again later please";
+      console.log("it reached here,", error);
+      toast.error(res);
+    }
   };
   return (
     <div className="container">
@@ -47,7 +74,7 @@ const Login = () => {
         <div className="card_terms">
           <span>
             Don't have an account yet?
-            <Link to="/signup">Login here</Link>
+            <Link to="/signup">signup here</Link>
           </span>
         </div>
       </div>
