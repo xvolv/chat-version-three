e27ccd7f32f9447f841bec56bc10f779ba50cef3
salhZvolv <nothingism99@gmail.com>
2025-03-12 10:35:04 +0300

    addig a loadding page while requesting

---
+++ client/src/apiCalls/components/loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="loader-overlay">
+      <div className="loader-spinner"></div>
+    </div>
+  );
+};
+
+export default Loader;
