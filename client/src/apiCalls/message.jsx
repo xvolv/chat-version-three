452d644638fad270b7cf53b1ef9c0c57f3f83513
salhZvolv <nothingism99@gmail.com>
2025-03-15 11:55:03 +0300

    message component a chat between two users

---
+++ client/src/apiCalls/message.jsx
@@ -0,0 +1,17 @@
+import { axiosInstance } from ".";
+
+export const createNewMessage = async (message) => {
+  try {
+    const response = await axiosInstance.post(
+      "api/message/new-message",
+      message
+    );
+
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      return error.response.data;
+    }
+    throw error;
+  }
+};
